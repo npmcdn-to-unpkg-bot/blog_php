1c64cad5c83315d2775d7ac197437d36077bd59d
DasonCheng <dasoncheng@foxmail.com>
2016-07-21 10:17:04 +0800

    autoHeight

--- entry/origin/js/base.js
+++ entry/origin/js/base.js
@@ -1,4 +1,11 @@
 $(function () {
+    if ($(window).height() > $(".pusher").height()) {
+        console.log($(window).height());
+        console.log($(".pusher header").height());
+        console.log($(".pusher footer").height());
+        console.log($(window).height() - $(".pusher header").height() - $(".pusher footer").height());
+        $(".pusher section").innerHeight($(window).height() - $(".pusher header").height() - $(".pusher footer").height());
+    }
     $("header #sidebar").click(function () {
         $('.ui.sidebar').sidebar('toggle');
     });
